4823ebdc656bf3c15665a0a0c1810d7a271989ca
Jaden Falbee <j.falbee04@gmail.com>
2023-07-06 20:47:31 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function findMatching(drivers, string) {
+    return drivers.filter(
+      (driver) => driver.toLowerCase() === string.toLowerCase()
+    );
+  }
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(
+        (maybeMatch) =>
+            maybeMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(record => record.name === string);
+}
